d63f90ec433a7d73b0ae5a15de9ff28ac2c0b522
JGSoftwareSolutions <jayonnet2003@yahoo.co.in>
2016-09-29 15:43:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,13 @@ require('./middleware/routes.js')(app,connection,io,Session,cookieParser,session
 
 /*
 	Running our application  
-*/
+
 http.listen(8080,function(){
     console.log("Listening on http://127.0.0.1:8080");
+});
+*/
+
+http.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
